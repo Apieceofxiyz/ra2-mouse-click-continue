387b5a9b70a3b63a33d7157bc1ff3ced525fa14a
lenchu <553308706@qq.com>
2021-02-21 21:45:33 +0800

    team control

---
+++ MouseClick/ISubscribe.cs
@@ -0,0 +1,14 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseClick
+{
+    public interface ISubscribe
+    {
+        void SubscribeHandler(bool subscribe, IKeyboardMouseEvents KMEvents);
+    }
+}
---
+++ MouseClick/plugins/TeamControlPlugin.cs
@@ -0,0 +1,55 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MouseClick.Form1;
+
+namespace MouseClick.plugins
+{
+    class TeamControlPlugin : ISubscribe
+    {
+        private bool Subscribed = false;
+
+        private IKeyboardMouseEvents KMEvents;
+
+        public void SubscribeHandler(bool subscribe, IKeyboardMouseEvents KMEvents)
+        {
+            try
+            {
+                if (subscribe)
+                {
+                    this.Subscribed = subscribe;
+                    this.KMEvents = KMEvents;
+                    this.KMEvents.KeyDown += KMEvents_KeyPress;
+                }
+                else
+                {
+                    this.KMEvents.KeyDown -= KMEvents_KeyPress;
+                    this.KMEvents = null;
+                }
+            }
+            catch { }
+        }
+
+        private void KMEvents_KeyPress(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine($"KeyCode: {e.KeyCode}, KeyValue: {e.KeyValue}, KeyData: {e.KeyData}");
+            if (e.KeyCode.ToString().Equals("Q"))
+            {
+                KeyboardSimulator.KeyPress(Keys.D6);
+            } else if (e.KeyCode.ToString().Equals("W"))
+            {
+                KeyboardSimulator.KeyPress(Keys.D7);
+            } else if (e.KeyCode.ToString().Equals("E"))
+            {
+                KeyboardSimulator.KeyPress(Keys.D8);
+            } else if (e.KeyCode.ToString().Equals("R"))
+            {
+                KeyboardSimulator.KeyPress(Keys.D9);
+            }
+        }
+    }
+}
