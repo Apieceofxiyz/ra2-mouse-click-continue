4e7a1b0580fd0ef6ed971c608c56e4e931d85624
lenchu <553308706@qq.com>
2021-07-24 18:30:32 +0800

    TeamControl按键映射改用配置文件映射按键

--- MouseClick/plugins/TeamControlPlugin.cs
+++ MouseClick/plugins/TeamControlPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using Gma.System.MouseKeyHook;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,31 @@ namespace MouseClick.plugins
 {
     class TeamControlPlugin : ISubscribe
     {
+        private string keyMappingFilePath = Path.Combine(Config.AppRoot, "plugins", "KeyMappings.txt");
+        private Dictionary<string, int> keyMapping;
+
         private bool Subscribed = false;
 
         private IKeyboardMouseEvents KMEvents;
+
+        public TeamControlPlugin()
+        {
+            string[] lines = File.ReadAllLines(keyMappingFilePath);
+            this.keyMapping = new Dictionary<string, int>(lines.Length);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                if (string.IsNullOrWhiteSpace(line) || line.Trim().StartsWith("//"))
+                {
+                    continue;
+                }
+                string[] oneMapping = line.Split('=');
+                if (oneMapping.Length != 2) continue;
+                string key = oneMapping[0];
+                string value = oneMapping[1];
+                keyMapping.Add(key, int.Parse(value));
+            }
+        }
 
         public void SubscribeHandler(bool subscribe, IKeyboardMouseEvents KMEvents)
         {
@@ -37,19 +60,24 @@ namespace MouseClick.plugins
         private void KMEvents_KeyPress(object sender, KeyEventArgs e)
         {
             Console.WriteLine($"KeyCode: {e.KeyCode}, KeyValue: {e.KeyValue}, KeyData: {e.KeyData}");
-            if (e.KeyCode.ToString().Equals("Q"))
+            string key = e.KeyCode.ToString();
+            if (keyMapping.ContainsKey(key))
             {
-                KeyboardSimulator.KeyPress(Keys.D6);
-            } else if (e.KeyCode.ToString().Equals("W"))
-            {
-                KeyboardSimulator.KeyPress(Keys.D7);
-            } else if (e.KeyCode.ToString().Equals("E"))
-            {
-                KeyboardSimulator.KeyPress(Keys.D8);
-            } else if (e.KeyCode.ToString().Equals("R"))
-            {
-                KeyboardSimulator.KeyPress(Keys.D9);
+                KeyboardSimulator.KeyPress((Keys)keyMapping[key]);
             }
+            //if (e.KeyCode.ToString().Equals("Q"))
+            //{
+            //    KeyboardSimulator.KeyPress(Keys.D6);
+            //} else if (e.KeyCode.ToString().Equals("W"))
+            //{
+            //    KeyboardSimulator.KeyPress(Keys.D7);
+            //} else if (e.KeyCode.ToString().Equals("E"))
+            //{
+            //    KeyboardSimulator.KeyPress(Keys.D8);
+            //} else if (e.KeyCode.ToString().Equals("R"))
+            //{
+            //    KeyboardSimulator.KeyPress((Keys)57);
+            //}
         }
     }
 }
